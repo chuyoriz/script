341f20fc5f346ce3b6315a779a8e296732e330d7
chuyoriz <izzanyustudio@gmail.com>
2025-07-09 02:01:50 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,9 +1,5 @@
-if game.PlaceId ~= 121864768012064 then
+if game.PlaceId == 121864768012064 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/chuyoriz/script/refs/heads/main/fishit.lua'))()
-    return
-end
-
-if game.PlaceId ~= 99567941238278 then
+elseif game.PlaceId == 99567941238278 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/chuyoriz/script/refs/heads/main/ink.lua'))()
-    return
 end
