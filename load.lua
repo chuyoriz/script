e393e42e403fb72a7c1e812b2dfc89b5774e09d0
chuyoriz <izzanyustudio@gmail.com>
2025-07-09 01:54:45 +0800

    Add files via upload

---
+++ load.lua
@@ -0,0 +1,9 @@
+if game.PlaceId ~= 121864768012064 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/chuyoriz/script/refs/heads/main/fishit.lua'))()
+    return
+end
+
+if game.PlaceId ~= 99567941238278 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/chuyoriz/script/refs/heads/main/ink.lua'))()
+    return
+end
